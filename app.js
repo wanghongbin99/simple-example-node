ddc5d18153fcab6174194112dc8b4b7cb344c777
Hongbin Wang <hongbin.wang@ebaotech.com>
2020-06-05 17:24:24 +0800

    Update my code to use PostgreSQL

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//database connection
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    connectionString: process.env.QOVERY_DATABASE_MY_DB_CONNECTION_URI,
+})
+
+
 var app = express();
 
 // view engine setup
@@ -15,7 +23,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
